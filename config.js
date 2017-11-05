9c2c4378ced4659ceb0bac49f9659ca5f554c619
mgrobelny <michalmaciejg@gmailcom>
2017-11-05 18:24:54 +0100

    init

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID' : '1028263550244-g8gug4ear6nmmt5q9n2tcl8dsho69piq.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET' : 'zHhpsY9Rdq781Ake_69oPUnt',
+	'CALLBACK_URL' : 'http://localhost:3000/auth/google/callback'
+};
+
